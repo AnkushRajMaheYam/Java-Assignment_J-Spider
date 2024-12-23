86f9705ea59a0d464897a2ef117c3cda2d591b7c
Ankush Raj Mahe Yam <ankushrajmaheyam@gmaill.com>
2024-12-23 22:05:01 +0530

    more pattern question added

---
+++ Assign24/Q66.java
@@ -0,0 +1,68 @@
+/*
+               1
+           2   3   4
+       5   6   7   8   9
+  10  11  12  13  14  15  16
+      17  18  19  20  21
+          22  23  24
+              25
+ */
+
+ import java.util.Scanner;
+
+ public class Q66 {
+     public static void pattern(int n){
+         int num=1;
+         int space=n/2;
+         int x = 1;
+         String s1 = " ";
+         int mid = n/2+1;
+ 
+         for(int i=1; i<=n; i++){
+             for(int j=1; j<=space; j++){
+                 System.out.printf("%4s",s1);
+             }
+             for(int j=1; j<=num; j++){
+                 System.out.printf("%4d",x);
+                 x++;
+             }
+
+             if(mid>i){
+                space--;
+                num+=2;
+             }
+             else{
+                space++;
+                num-=2;
+             }
+             
+             System.out.println();
+         }
+ 
+     }
+ 
+     public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+         System.out.print("Enter a digit : ");
+         int n = sc.nextInt();
+         if(n%2==0){
+            System.out.println("Please enter only odd value.");
+            return;
+        }
+         pattern(n);
+     }
+ }
+ 
+ 
+  /*
+   * More Pattern Based Questions uploaded in this folder link :-
+   * https://github.com/AnkushRajMaheYam/Java-Assignment_J-Spider/tree/main/
+   * Assign24
+   * 
+   * // Folder Structure :-
+   * Java-Assignment_J-Spider/
+   * ├── Assign24/
+   * └── Q66.java
+   * 
+   * 
+   */
---
+++ Assign24/Q67.java
@@ -0,0 +1,80 @@
+
+import java.util.Scanner;
+
+public class Q67 {
+    public static  void pattern(int n){
+        int star=1;
+        int space=n/2;
+        int mid = n/2+1;
+        int mSstar=n+2;
+        int mEstar=2*n-1;
+
+        for(int i = 1; i<=n; i++){
+            for(int j=1; j<=space; j++){
+                System.out.print("  ");
+            }
+            for(int j=1; j<=star; j++){
+                System.out.print("* ");
+            }
+
+            for(int j=mid+1; j<=n+1; j++){
+                if((i==1 || j==n+1) && i<=mid )
+                System.out.print("@ ");
+                else{
+                    System.out.print("  ");
+                }
+            }
+
+            for(int j = n+2; j<=2*n-1; j++){
+                if(i<=mid || j<mSstar || j>mEstar){
+                    System.out.print("  ");
+                }
+                else{
+                    System.out.print("* ");
+                }
+                
+            }
+
+            for(int j=2*n; j<=(2*n)+(n/2); j++){
+                if(i<=mid && (i==1 || j==2*n) )
+                System.out.print("@ ");
+                else{
+                    System.out.print("  ");
+                }
+            }
+
+            for(int j=1; j<=star; j++){
+                System.out.print("* ");
+            }
+
+            if(mid>i){
+                star++;
+                space--;
+                
+            }
+            else{
+                star--;
+                space++;
+            }
+            if(mid<i){
+                mSstar++;
+                mEstar--;
+            }
+
+            System.out.println();
+        }
+
+    }
+
+
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        int n = sc.nextInt();
+        if(n%2==0){
+            System.out.println("Please enter only odd value.");
+            return;
+        }
+        pattern(n);
+    }
+}
